f834cae183b914bdaf819d68bcf3b10549f1f829
unknown <ptsolmyr@.(none)>
2010-09-08 22:05:53 +0800

    change files name in the original directory

--- changeFilesName.py
+++ changeFilesName.py
@@ -1,12 +1,28 @@
 #! /bin/python
 
-import os, glob
+import os, glob, sys, shutil
 
-path = r'/home/solmyr/python/changeFilesName/test'
+path = os.getcwd()
+
+newExt = 'log'
+
+argc = len(sys.argv)
+
+if(argc > 1) :
+    path = sys.argv[1]
+
+if(argc > 2) :
+    newExt = sys.argv[2]
 
 for file in glob.glob(os.path.join(path, "*")) :
+    #path = os.path.dirname(file)
+    #filename = os.path.basename(file)
     name, ext = os.path.splitext(file)
     print "name: " + name + "\text: " + ext
-    newFileName = name + '.log'
+    newFileName = name + '.' + newExt
+    if file == newFileName :
+	continue
+    #os.mkdir("..\logFiles");
+    #shutil.copyfile(file, "../logFiles/" + newFileName)
     os.rename(file, newFileName)
 
